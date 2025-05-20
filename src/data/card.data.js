219fc07b3c7223104f4af7eec27409018e29392c
ralf <chrystianralfc@gmail.com>
2025-05-19 21:21:25 -0400

    add information of menbresia

---
+++ src/data/card.data.js
@@ -0,0 +1,34 @@
+export const cards = [
+  {
+    title: "Pase 'Prueba'",
+    price: 0,
+    descList: [
+      "Acceso al gimnasio de 08:00 a 14:00", "El entrenador de turno te presentará el gimnasio"
+    ]
+  },
+  {
+    title: "Pase 'Inicio Fácil'",
+    price: 119,
+    descList: ["Visitas sin restricciones 24/7", "Programa de entrenamiento individual", "Acceso a la aplicación VTRAINER", "Soporte del entrenador"]
+  },
+  {
+    title: "Pase 'Tiempo Libre'",
+    price: 49,
+    descList: ["El horario de entrada al gimnasio es de 14:00 a 18:00", "Sin suspensión de membresía", "El entrenador de turno te presentará el gimnasio"]
+  },
+  {
+    title: "Pase '1 Mes 24/7'",
+    price: 85,
+    descList: ["Visitas sin restricciones 24/7", "El entrenador de turno te presentará el gimnasio"]
+  },
+  {
+    title: "Pase 'En Forma (AM)'",
+    price: 165,
+    descList: ["Entrenamiento en mini-grupos hasta las 14:00", "Sin suspensión de membresía", "Clases con entrenador en mini-grupo 3 veces por semana", "Soporte del entrenador"]
+  },
+  {
+    title: "Pase 'En Forma (PM)'",
+    price: 195,
+    descList: ["Entrenamiento en mini-grupos hasta las 17:00", "Sin suspensión de membresía", "Clases con entrenador en mini-grupo 3 veces por semana", "Soporte del entrenador"]
+  }
+]
